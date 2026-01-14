dd34508cbbb67ce9c9124d26e559fba12b1f8eeb
Alejandro Cortes <alex.cortes.i10@gmail.com>
2026-01-14 14:57:57 -0600

    small commit

--- openai_intro/openai-simple.py
+++ openai_intro/openai-simple.py
@@ -9,8 +9,12 @@ client = OpenAI(api_key= os.getenv("OPEN_AI_KEY"))
 response = client.chat.completions.create(
     model='gpt-4o-mini',
     messages = [
-        {'role': "system", "content": "You are a helpfu assistant."},
-        {'role': "user", "content": "What is the purpose in life?"}
+        # {'role': "system", "content": "You are a helpful assistant."},
+        # {'role': "user", "content": "What is the purpose in life?"},
+        {'role': "system", "content": "You are eastern poet."},
+        {'role': "user", "content": "Write me a short poem about the moon. \
+                Write the poem in the style of haiku. \
+                Make sure to include a title."}
     ],
     max_tokens = 100
 )
