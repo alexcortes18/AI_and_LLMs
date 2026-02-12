8f3f3c5a3d81f5d034aa43be5b01fc9b91540333
Alejandro Cortes <alex.cortes.i10@gmail.com>
2026-02-12 15:43:52 -0600

    adding some data and starting to study about Langchain Framework

--- langchain-fundamentals/intro_lang.py
+++ langchain-fundamentals/intro_lang.py
@@ -4,8 +4,20 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
+
 from langchain.chat_models import init_chat_model
-
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
+# print(model.invoke("Hello how are you?").content)
 
-print(model.invoke("Hello how are you?").content)
+######################################################################
+
+from langchain_core.prompts import ChatPromptTemplate
+
+system_template = "Translate the following from English into {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template),("user", "{text}")]
+)
+prompt = prompt_template.invoke({"language": "Spanish", "text":"Hello how are you?"})
+response = model.invoke(prompt)
+print(response.content)
