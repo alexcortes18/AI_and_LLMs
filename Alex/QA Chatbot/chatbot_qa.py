f9f515a3941a98abcbe9a43e54ca7318f385275c
Alejandro Cortes <alex.cortes.i10@gmail.com>
2026-02-27 11:43:42 -0600

    moved a lot of folders into better positions and fixed the venv

--- Alex/QA Chatbot/chatbot_qa.py
+++ Alex/QA Chatbot/chatbot_qa.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from lanchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import SeleniumURLLoader
