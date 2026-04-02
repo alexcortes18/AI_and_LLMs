7bd7b8f66643a70f6877e0a26954514d18d410e9
Alejandro Cortes <alex.cortes.i10@gmail.com>
2026-04-02 13:04:03 -0600

    fine-tuned for the first time with openai

--- Alex/Fine_Tunning/ft_openai.py
+++ Alex/Fine_Tunning/ft_openai.py
@@ -37,9 +37,11 @@ def json_to_jsonl(input_file, output_file):
     # produce JSONL from JSON
     with open(output_file, "w") as outfile:
         for entry in data:
+            # print(entry)
             json.dump(entry, outfile)
             outfile.write("\n")
             
+# To check whether your fine-tuning training data has the expected chat format before you upload it. 
 def check_file_format(dataset):
     # Format error checks
     format_errors = defaultdict(int)
@@ -86,3 +88,112 @@ def check_file_format(dataset):
             print(f"{k}: {v}")
     else:
         print("No errors found")
+        
+# To count the estimated toke usage for OpenAI API costs.
+# simplified from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+def num_tokens_from_messages(messages, tokens_per_message=3, tokens_per_name=1):
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3
+    return num_tokens
+
+json_to_jsonl("Alex/Fine_Tunning/teachrafter.json","Alex/Fine_Tunning/output.jsonl")
+
+# Check file format:
+data_path = "Alex/Fine_Tunning/output.jsonl"
+
+# Load the dataset from:https://cookbook.openai.com/examples/chat_finetuning_data_prep
+with open(data_path, "r", encoding="utf-8") as f:
+    dataset = [json.loads(line) for line in f]
+    
+# Initial dataset stats:
+# print("Num examples:", len(dataset))
+# print("First example:")
+# for message in dataset[0]["messages"]:
+    # print(message)
+    
+check_file_format(dataset)
+# print(f"Length of the dataset is: {len(dataset)}")
+
+# Cost estimations
+# Get the length of the conversation
+conversation_length = []
+
+for msg in dataset:
+    messages = msg["messages"]
+    conversation_length.append(num_tokens_from_messages(messages))
+
+# Pricing and default n_epochs estimate
+MAX_TOKENS_PER_EXAMPLE = 4096
+TARGET_EPOCHS = 5
+MIN_TARGET_EXAMPLES = 100
+MAX_TARGET_EXAMPLES = 25000
+MIN_DEFAULT_EPOCHS = 1
+MAX_DEFAULT_EPOCHS = 25
+
+n_epochs = TARGET_EPOCHS
+n_train_examples = len(dataset)
+
+if n_train_examples * TARGET_EPOCHS < MIN_TARGET_EXAMPLES:
+    n_epochs = min(MAX_DEFAULT_EPOCHS, MIN_TARGET_EXAMPLES // n_train_examples)
+elif n_train_examples * TARGET_EPOCHS > MAX_TARGET_EXAMPLES:
+    n_epochs = max(MIN_DEFAULT_EPOCHS, MAX_TARGET_EXAMPLES // n_train_examples)
+
+n_billing_tokens_in_dataset = sum(
+    min(MAX_TOKENS_PER_EXAMPLE, length) for length in conversation_length
+)
+print(
+    f"Dataset has ~{n_billing_tokens_in_dataset} tokens that will be charged for during training"
+)
+print(f"By default, you'll train for {n_epochs} epochs on this dataset")
+print(
+    f"By default, you'll be charged for ~{n_epochs * n_billing_tokens_in_dataset} tokens"
+)
+
+num_tokens = n_epochs * n_billing_tokens_in_dataset
+
+# Current training estimate using gpt-4o-mini-2024-07-18 pricing: $3.00 / 1M tokens
+TRAINING_MODEL = "gpt-4o-mini-2024-07-18"
+TRAINING_COST_PER_1M_TOKENS = 3.00
+
+cost = (num_tokens / 1_000_000) * TRAINING_COST_PER_1M_TOKENS
+print(f"Estimated training cost for {TRAINING_MODEL}: ${cost:.6f} USD")
+
+
+# UPLOAD FILES TO OPENAI:
+# Upload file once all validations are successful!
+# Each time you run this code, it will create another file in your platform.openai.com account, 
+# in your Storage section.
+# ---------------------------------------------------------------------------------------------------------------------------------
+# training_file = ""
+# training_file = client.files.create(
+#     file=open(data_path, "rb"), purpose="fine-tune"
+# )
+# print(f"Training file id: {training_file.id}")
+# ---------------------------------------------------------------------------------------------------------------------------------
+
+# == Next steps: Create a fine-tuned model ===
+# Start the fine-tuning job
+# After you've started a fine-tuning job, it may take some time to complete. Your job may be queued
+# behind other jobs and training a model can take minutes or hours depending on the
+# model and dataset size.
+# ---------------------------------------------------------------------------------------------------------------------------------
+# response = client.fine_tuning.jobs.create(
+#     training_file=training_file.id,
+#     model=TRAINING_MODEL,
+#     hyperparameters={
+#         "n_epochs": 5
+#     }
+# )
+# print(f"Response id from creating the job: {response.id}") 
+# ---------------------------------------------------------------------------------------------------------------------------------
+# Retrieve the state of a fine-tune
+# Status field can contain: running or succeeded or failed, etc.
+# state = client.fine_tuning.jobs.retrieve(response.id) #ftjob-0w4vss9dD82XinmEKTpGxrab
+state = client.fine_tuning.jobs.retrieve("ftjob-0w4vss9dD82XinmEKTpGxrab")
+pprint.pprint(f"Fine-tuning job is running{state}")
