aad231eabbe88dd4691631eb41fe1cc163fbe20f
Alejandro Cortes <alex.cortes.i10@gmail.com>
2026-04-03 22:53:27 -0600

    compute metrics function

--- Alex/fine-tuning-lora/lora_text_simpler.py
+++ Alex/fine-tuning-lora/lora_text_simpler.py
@@ -135,19 +135,26 @@ if __name__ == "__main__":
         cfg["train_data"].set_format("torch") 
         cfg["test_data"].set_format("torch")
         
-        training_arguments = TrainingArguments(
-            output_dir="./checkpoints",
-            learning_rate=2e-4,
-            per_device_eval_batch_size=16,
-            per_device_train_batch_size=16,
-            num_train_epochs=5,
-            weight_decay=0.01,
-            evaluation_strategy="epoch",
-            save_strategy="epoch",
-            load_best_model_at_end=True,
-            metric_for_best_model="accuracy",
-            greater_is_better=True,
-            logging_steps=10,
-            warmup_steps=10,
-            seed=42
-        )
+    training_arguments = TrainingArguments(
+        output_dir="./checkpoints",
+        learning_rate=2e-4,
+        per_device_eval_batch_size=16,
+        per_device_train_batch_size=16,
+        num_train_epochs=5,
+        weight_decay=0.01,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+        metric_for_best_model="accuracy",
+        greater_is_better=True,
+        logging_steps=10,
+        warmup_steps=10,
+        seed=42
+    )
+    
+    metric = evaluate.load("accuracy")
+    
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return metric.compute(predictions=predictions, references=labels)
